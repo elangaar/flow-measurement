3bef13cdf62c201a90dbb5203ee51711007696a6
elangaar <elangaar@gmail.com>
2017-04-22 16:37:57 +0200

    configured login template

--- flow/urls.py
+++ flow/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from allauth.account import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(
-        template_name="flow_measurement/index.html"),
-        name='index'),
+    url(r'^$', views.login, name='index'),
     url(r'^accounts/', include('allauth.urls')),
 ]
